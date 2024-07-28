24f33987805cec30c84b6492a073699749583caa
the-avinash-singh <abhinavarya87012@gmail.com>
2024-07-28 08:29:25 +0530

    70% done

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,27 +1,30 @@
-import React from 'react'
+import React from "react";
+import { FaArrowRight } from "react-icons/fa";
+import "../stylesheet/skills.css";
 
 const Skills = () => {
   return (
     <>
-    <div className='card shadow services-card'>
-      <div className='mx-3 my-2'>
-      <h3>Skills</h3>
-      <div className='my-2'>
-      <h5 className='text-muted'>Product Management</h5>
-      <div className='text-muted border-bottom'>
-        organaization name
-      </div>
-      </div>
-      <div className='my-2'>
-      <h5 className='text-muted'>Product Management</h5>
-      <div className='text-muted'>
-        organaization name
-      </div>
-      </div>
-      </div>
+      <div className="card shadow skill-card">
+        <div className="mx-3 my-2">
+          <h3>Skills</h3>
+          <div className="skill-list">
+            <div className="mt-2 pb-2">
+              <h5 className="text-muted">Product Management</h5>
+              <div className="text-muted">organaization name</div>
+            </div>
+            <div className="mt-2 pb-2">
+              <h5 className="text-muted">Certificate name</h5>
+              <div className="text-muted">organaization name</div>
+            </div>
+          </div>
+        </div>
+        <div className=" card-footer text-center text-muted fw-medium">
+          Show all skills <FaArrowRight />
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
--- src/components/languages.jsx
+++ src/components/languages.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import "../stylesheet/language.css"
 
 const Languages = () => {
   return (
-    <div className='card shadow about-card'>
+    <div className='card shadow lang-card'>
       <div className='mx-3'>
       <h3 className='my-2'>Languages</h3>
       <div className='mb-2 fw-medium'>
