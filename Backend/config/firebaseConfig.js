149c243286389093e98bffba92508420d5fd00ef
the-avinash-singh <abhinavarya87012@gmail.com>
2024-08-28 17:14:27 +0530

    backend and frontend's alignment

--- Backend/config/firebaseConfig.js
+++ Backend/config/firebaseConfig.js
@@ -3,7 +3,7 @@ const serviceAccount = require('../linkedin-profile.json')
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://linkedin-profile-generator-api.firebaseio.com" // Replace with your database URL
+  databaseURL: process.env.DATABASE_URL 
 });
 
 const db = admin.firestore();
